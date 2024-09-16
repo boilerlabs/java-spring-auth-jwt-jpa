e01fd99e84e220565efe57364a3678b573cfc1b7
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-09-16 20:37:33 -0300

    feat: update UserRepository
    added findByUsername and existsByUsername methods

--- src/main/java/com/boilerlabs/auth/repositories/UserRepository.java
+++ src/main/java/com/boilerlabs/auth/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.boilerlabs.auth.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.boilerlabs.auth.entities.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
