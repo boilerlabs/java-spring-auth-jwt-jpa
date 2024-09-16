2de17f0a14955fa994ae9231eb2b64df6d9e3f30
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-09-16 17:22:42 -0300

    chore: init project

---
+++ src/main/java/com/boilerlabs/auth_jwt_java_spring/AuthJwtJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.boilerlabs.auth_jwt_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boilerlabs/auth_jwt_java_spring/AuthJwtJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boilerlabs.auth_jwt_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
