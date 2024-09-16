57fff0c8a5cabf7885d00a6266240813e56ebd34
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-09-16 18:48:29 -0300

    style: rn Application

--- src/main/java/com/boilerlabs/auth_jwt_java_spring/Application.java
+++ src/main/java/com/boilerlabs/auth_jwt_java_spring/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthJwtJavaSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthJwtJavaSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
